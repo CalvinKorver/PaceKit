d3c01b572257109efc3526e732b465beebeb7b06
calvinkorver <calvin.korver@gmail.com>
2025-01-28 17:20:42 -0500

    merged cleanup

--- WatchWorkoutSync/Views/Helpers/SceneDelegate.swift
+++
@@ -1,9 +0,0 @@
-// SceneDelegate.swift or equivalent
-func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
-    // Enable Watch connectivity
-    if WCSession.isSupported() {
-        let session = WCSession.default
-        session.delegate = self
-        session.activate()
-    }
-}
